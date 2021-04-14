d16658721498bd836e84f0ba48e39d6f9f911f9f
github username <jhernandez1692@gmail.com>
2021-04-14 16:04:52 -0400

    Include routes for Hero#index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :comments, only: [:index, :create]
+      resources :heros, only: [:index]
     end
   end
 end
