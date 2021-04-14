3e91f1d27e8f1001da1eb261a6c6f84faa39c2bd
github username <jhernandez1692@gmail.com>
2021-04-14 15:58:01 -0400

    Include create action and strong params.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,4 +6,21 @@ class Api::V1::CommentsController < ApplicationController
         render json: comments
     end
 
+    def create
+        comment = Comment.new(comment_params)
+        if comment.save
+            render json: comment, status: :accepted
+        else
+            render json: {errors: comment.errors.full_messages} status: :unprocessable_entity
+    end
+
+    
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :hero_id)
+    end
+
+
 end
